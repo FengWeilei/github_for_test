0c66a797a4c11e01b7973bbfdf8b0e70731c23d7
MY <18790166674@163.com>
2017-06-28 06:17:34 -0700

    added myflaskapp-version4

---
+++ myflaskapp-version4/data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title':'Article One',
+			'body':'This is article one.I will rewrite it later.',
+			'author': 'FengWeilei',
+			'creat_date':'26-06-2017'
+		},
+		{
+			'id': 2,
+			'title':'Article Two',
+			'body':'This is article two.I will rewrite it later.',
+			'author': 'FengWeilei',
+			'creat_date':'26-06-2017'
+		},
+		{
+			'id': 3,
+			'title':'Article Three',
+			'body':'This is article three.I will rewrite it later.',
+			'author': 'FengWeilei',
+			'creat_date':'26-06-2017'
+		}
+	]
+	return articles
